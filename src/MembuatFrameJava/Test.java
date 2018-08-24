1250aaac01ad542e6bbc95d53397b1d323dd74b3
admin <admin@4c>
2018-08-24 11:39:07 +0700

    JFrame 0.0

---
+++ src/MembuatFrameJava/Test.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MembuatFrameJava;
+//@author damario_aditamaa
+
+import javax.swing.JFrame;
+
+
+public class Test {
+
+    public static void main(String[] args) {
+        JFrame test = new JFrame();
+        test.setSize(300,400);
+        test.setTitle("JFrame 0.0");
+        test.setVisible(true);
+    }
+
+}
